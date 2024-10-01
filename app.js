77942170713ac9477ac018229282f56857ba5960
Darren <darrensheftic@yahoo.com>
2024-10-01 15:24:04 -0400

    Module 5 Completed Baseline

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ var logger = require('morgan');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index');
+
 var handlebars = require('hbs');
+
+// Bring in Database
+require('./app_api/models/db');
 
 var app = express();
 
@@ -15,7 +20,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 
 // register handlebars partials (https://www.npmjs.com/package/hbs)
-handlebars.registerPartials(__dirname + './app_server/views/partials');
+handlebars.registerPartials(__dirname + '/app_server/views/partials');
 
 app.set('view engine', 'hbs');
 
@@ -28,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
-
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,10 +1,37 @@
-var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('./data/trips.json',
-'utf8'));
+const tripsEndpoint = 'http://localhost:3000/api/trips';
+const options = {
+method: 'GET',
+headers: {
+'Accept': 'application/json'
+}
+}
+
+// var fs = require('fs');
+// var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
 /* GET travel view */
-travel = (req,res) =>{
-    res.render('travel', {title: 'Travlr Getaways', trips});
+const travel = async(req,res,next) => {
+    //console.log('TRAVEL CONTROLLER BEGIN')
+    await fetch(tripsEndpoint, options)
+        .then(res => res.json())
+        .then(json => {
+            // console.log(json);
+            let message = null;
+            if(!(json instanceof Array)) {
+                message = 'API Lookup Error';
+                json = [];
+            } else {
+                if(!json.length){
+                    message = 'No Trips Exist In Our Database!';
+                }
+            }
+            res.render('travel', {title: 'Travlr Getaways', trips: json, message});
+        })
+        .catch(err => res.status(500).send(e.message));
+    // console.log('TRAVEL CONTROLLER AFTER RENDER')
 };
+
+
 
 module.exports = {
     travel
